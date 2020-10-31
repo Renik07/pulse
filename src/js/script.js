b1b5f49384d87eb7d1ac7862d8e89b7fd23a6a54
Renat G <restspread@gmail.com>
2020-11-01 00:10:51 +0300

    feat: add smooth scroll and pageup

--- src/js/script.js
+++ src/js/script.js
@@ -48,10 +48,6 @@ $(document).ready(function(){
 			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
 		});
 	});
-	/* $('.button_submit').on('click', function() {
-		$('#order').fadeOut();
-		$('#thanks').fadeIn();
-	}); */
 
 	/* form validation */
 	function validateForms(form) {
@@ -80,6 +76,36 @@ $(document).ready(function(){
 	validateForms('#order form');
 
 	/* input number mask */
-
 	$('input[name=phone]').mask("+7(999) 999-99-99");
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+		$.ajax({
+			type: 'POST',
+			url: 'mailer/smart.php',
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find('input').val('');
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn();
+			$('form').trigger('reset');
+		});
+		return false;
+	});
+
+	/* scroll and pageup */
+	$(window).scroll(function() {
+		if($(this).scrollTop() > '1200') {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$("a[href^='#']").click(function(){
+		const _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
+
 });
